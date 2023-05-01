6c01f2cffcea51b7af1e551e37655b2fb36d6365
SeKiZo0 <105446823+SeKiZo0@users.noreply.github.com>
2023-05-01 22:07:45 +0200

    changed layout and added website

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,6 @@
 //scroll tracker api
 import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
 
-/*this is to highlight the link you are currently on*/
-$('body').scrollspy({target: ".navbar"});
-
 $(window).scroll(function() {
   parallax();
 })
@@ -11,7 +8,7 @@ $(window).scroll(function() {
 function parallax() {
   var wScroll = $(window).scrollTop();
 
-  $('.IntroText').css('margin-bottom', -(wScroll*0.35)+'px');
+  $('.IntroText').css('margin-bottom', -(wScroll*0.1)+'px');
 
 }
 
@@ -33,5 +30,49 @@ stars2.animate(
   }
 )
 
-document.querySelector('#about_me').textContent = 'ggez';
-document.querySelector('#about_me').style.color = 'blue';
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
+
+/* Main navigation */
+let panelsSection = document.querySelector("#panels"),
+	panelsContainer = document.querySelector("#panels-container"),
+	tween;
+  /*this is selecting all the anchors*/
+document.querySelectorAll(".anchor").forEach(anchor => {
+	anchor.addEventListener("click", function(e) {
+		e.preventDefault();
+    /*this targets all the listed ids */
+		let targetElem = document.querySelector(e.target.getAttribute("href")),
+			y = targetElem;
+		if (targetElem && panelsContainer.isSameNode(targetElem.parentElement)) {
+			let totalScroll = tween.scrollTrigger.end - tween.scrollTrigger.start,
+				totalMovement = (panels.length - 1) * targetElem.offsetWidth;
+			y = Math.round(tween.scrollTrigger.start + (targetElem.offsetLeft / totalMovement) * totalScroll);
+		}
+		gsap.to(window, {
+			scrollTo: {
+				y: y,
+				autoKill: false
+			},
+			duration: 1
+		});
+	});
+});
+
+/* Panels */
+const panels = gsap.utils.toArray("#panels-container .panel");
+tween = gsap.to(panels, {
+	xPercent: -100 * ( panels.length - 1 ),
+	ease: "none",
+	scrollTrigger: {
+		trigger: "#panels-container",
+		pin: true,
+		start: "top top",
+		scrub: 1,
+		snap: {
+			snapTo: 1 / (panels.length - 1),
+			inertia: false,
+			duration: {min: 0.1, max: 0.1}
+		},
+		end: () =>  "+=" + (panelsContainer.offsetWidth - innerWidth)
+	}
+});
