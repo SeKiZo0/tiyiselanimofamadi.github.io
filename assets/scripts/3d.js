90be905d48dbeb88406008de4d0e534af9d3d19d
SeKiZo0 <105446823+SeKiZo0@users.noreply.github.com>
2023-12-02 00:17:02 +0200

    Changed some imports

--- assets/scripts/3d.js
+++ assets/scripts/3d.js
@@ -1,7 +1,6 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js';
+import * as THREE from 'https://cdn.skypack.dev/three@0.133.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.133.0/examples/jsm/loaders/GLTFLoader.js';
+
 
 const sizes = {
     width: window.innerWidth,
